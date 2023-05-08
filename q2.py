da0568d9be2e305d3ea4e6e544caf58262a429a1
Rom Erez <rerez2025@kehillahstudent.org>
2023-05-08 11:45:52 -0700

    May 8

--- q2.py
+++ q2.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-url = 'https://en.wikipedia.org/wiki/Minnesota'
+url = 'https://en.wikipedia.org/wiki/New_York_City#Demographics'
 dfs = pd.read_html(
     url,
-    match = "Largest cities or towns in Minnesota"
+    match = "Historical population"
 )
 df = dfs[0]
-# df = df.iloc[0:-2]
-# df = df[ ['Name','Pop.'] ].astype('int')
-# print(df.info())
-# df.plot(x = 'Name.', y = 'Pop.', kind = 'bar')
-# plt.show()
+df = df.iloc[0:-2]
+
+df = df[['Year','Pop.']].astype('int')
+print(df)
+df.plot('Year.', y = 'Pop.', kind = 'bar')
+plt.show()
 print(df.info())
